e3366201f54247f34a6c67da917830b4870cc8ee
jvnakad <112445763+jvnakad@users.noreply.github.com>
2025-11-21 11:57:23 +0000

    add `/api/v1/status` endpoint

--- models/calculadora.js
+++ models/calculadora.js
@@ -1,8 +1,8 @@
-function somar(numero1, numero2) {
-  if (typeof numero1 !== "number") {
-    return "Erro";
-  }
-  return numero1 + numero2;
-}
+// function somar(numero1, numero2) {
+//   if (typeof numero1 !== "number") {
+//     return "Erro";
+//   }
+//   return numero1 + numero2;
+// }
 
-exports.somar = somar;
+// exports.somar = somar;
--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -16,14 +16,14 @@
 //   expect(1).toBe(1);
 // });
 
-const calculadora = require("../../models/calculadora.js");
+// const calculadora = require("../../models/calculadora.js");
 
-test("somar 2+2 deveria retornar 4", () => {
-  const resultado = calculadora.somar(2, 2);
-  expect(resultado).toBe(4);
-});
+// test("somar 2+2 deveria retornar 4", () => {
+//   const resultado = calculadora.somar(2, 2);
+//   expect(resultado).toBe(4);
+// });
 
-test("somar 'banana' + 2 deveria retornar 'Erro'", () => {
-  const resultado = calculadora.somar("banana", 2);
-  expect(resultado).toBe("Erro");
-});
+// test("somar 'banana' + 2 deveria retornar 'Erro'", () => {
+//   const resultado = calculadora.somar("banana", 2);
+//   expect(resultado).toBe("Erro");
+// });
